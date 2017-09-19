481e84682d35fc14cdb6175f117875b0f851c4cc
wshuang6 <seraph617@gmail.com>
2017-09-19 14:09:01 -0400

    Successfully making API call, getting data

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-console.log('hi world')
+const client = {
+  id: 'FKQRUZSHMVTK3YTZO1P1PZSEF54T54ECDI1BFMM55NPCAYS4',
+  secret: 'JEAPW3RDIAO0D3I32X225WXNSPN3DE5YJEUQYOLLKZA4LTC2',
+  exploreURL: 'https://api.foursquare.com/v2/venues/explore'
+}
+
+function exploreAPICall() {
+  const query = {
+    client_id: client.id,
+    client_secret: client.secret,
+    near: 'Washington, DC',
+    v: '20170919'
+  }
+  $.getJSON(client.exploreURL, query, function cb(results) {
+    // console.log(results)
+    let listOfResults = results.response.groups[0].items;
+    listOfResults.forEach(item => {
+      // console.log(item)
+      let photoURL;
+      if (item.tips[0].photo) {
+        photoURL = item.tips[0].photourl;
+      }
+      let name = item.venue.name;
+      let location = item.venue.location.address;
+      let locationURL = item.venue.url;
+      console.log(photoURL)
+      console.log(name)
+      console.log(location)
+      console.log(locationURL)
+    })
+  })
+}
+
+$(exploreAPICall);
